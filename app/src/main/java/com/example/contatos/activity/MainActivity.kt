b5809710dc106219a8ebc37fe8eedf5076ae35ed
Wendel <wendelljuliao@hotmail.com>
2021-05-24 14:33:05 -0300

    cardview de tasks

--- app/src/main/java/com/example/contatos/activity/MainActivity.kt
+++ app/src/main/java/com/example/contatos/activity/MainActivity.kt
@@ -6,42 +6,63 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.contatos.R
+import com.example.contatos.adapter.TaskAdapter
 import com.example.contatos.dao.ContatosDatabase
 import com.example.contatos.dao.TaskDAO
 import com.example.contatos.dao.UserDAO
 import com.example.contatos.model.Task
 import com.example.contatos.model.User
+import com.example.contatos.model.UserWithTasks
 import com.example.contatos.util.DatabaseUtil
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var taskDAO: TaskDAO
+
     private lateinit var userDAO: UserDAO
-    private lateinit var mMessage: TextView
+    private lateinit var mTaskList: RecyclerView
+    private lateinit var mUserWithTasks: UserWithTasks
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userDAO = DatabaseUtil.getInstance(applicationContext).getUserDAO()
+        taskDAO = DatabaseUtil.getInstance(applicationContext).getTaskDAO()
 
-        mMessage = findViewById(R.id.main_textview_message)
+        userDAO = DatabaseUtil.getInstance(applicationContext).getUserDAO()
+        mTaskList = findViewById(R.id.main_recyclerview_tasks)
+
+        var userId = -1
 
         if (intent != null) {
-            GlobalScope.launch {
-                val uid = intent.getIntExtra("userId", -1)
-                if (uid != -1) {
+            userId = intent.getIntExtra("userId", -1)
 
-                    val user = userDAO.find(uid)
+        }
 
-                    Handler(Looper.getMainLooper()).post {
-                        mMessage.text = user.firstName
-                    }
+        GlobalScope.launch {
 
+            val task1 = Task(name="Lavar louça", description = "aaa", isDone = false, userId = 1)
+            val task2 = Task(name="Lavar louça2", description = "aaa", isDone = true, userId = 1)
+
+            taskDAO.insert(task1)
+            taskDAO.insert(task2)
+
+
+            if (userId != -1) {
+                mUserWithTasks = userDAO.findTasksByUser(userId)
+                val taskAdapter = TaskAdapter(mUserWithTasks.tasks)
+                val llm = LinearLayoutManager(applicationContext)
+
+                mTaskList.apply {
+                    adapter = taskAdapter
+                    layoutManager = llm
                 }
             }
         }
---
+++ app/src/main/java/com/example/contatos/adapter/TaskAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.contatos.adapter
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contatos.R
+import com.example.contatos.model.Task
+
+class TaskAdapter(val tasks:List<Task>):RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.name.text = tasks[position].name
+        holder.description.text = tasks[position].description
+
+        if (tasks[position].isDone) {
+            holder.isDone.setBackgroundColor(Color.GREEN)
+        } else {
+            holder.isDone.setBackgroundColor(Color.RED)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return tasks.size
+    }
+
+    class TaskViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        val name: TextView = itemView.findViewById(R.id.item_task_textview_name)
+        val description: TextView = itemView.findViewById(R.id.item_task_textview_description)
+        val isDone: View = itemView.findViewById(R.id.item_task_view_isfinished)
+
+
+    }
+
+}
